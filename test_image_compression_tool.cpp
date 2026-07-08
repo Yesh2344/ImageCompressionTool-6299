8e59f4156a187b90be41e3b7fdba7d9e6061dca6
Yesh2344 <125935833+Yesh2344@users.noreply.github.com>
2026-07-08 08:02:22 +0000

    small cleanup pass

--- test_image_compression_tool.cpp
+++ test_image_compression_tool.cpp
@@ -6,5 +6,6 @@ TEST(ImageCompressionToolTest, CompressImage) {
     ImageCompressionTool::compress_image("input_image.jpg", "output_image.jpg", 90);
 
     // Check the output image file
+// minor polish
     ASSERT_TRUE(std::filesystem::exists("output_image.jpg"));
 }
